6cc765641cdbfe8d82728073916913739482933e
rubrubpowah123 <jetite@gmail.com>
2022-01-01 20:11:48 +1300

    Update idiot.js

--- idiot.js
+++ idiot.js
@@ -14,22 +14,49 @@ let windowWidth = 400;
 let windowHeight = 100;
 let mainWindow = null;
 let windowX = 0;
+let windowY = 0;
+
+let moveX = 2;
+let moveY = 2;
 
 function showDomWindow() {
 	mainWindow = window.open("", windowName, "width=" + windowWidth + ",height=" + windowHeight);
-	mainWindow.document.write("<p>This is " + windowName + ". I am " + windowWidth + "px wide and " + windowHeight + "px tall!</p>");	
+	// mainWindow.document.write("<p>This is " + windowName + ". I am " + windowWidth + "px wide and " + windowHeight + "px tall!</p>");	
+	mainWindow.document.write("<p>YOU ARE THE IDIOT</p>");	
 }
 
 function setup() {
-
+	moveX = random(2, 8);
+	moveY = random(2, 8);
 }
 
 // let counter = 0;
 function draw() {
 	// counter = counter + 1;
 	// console.log("hello: " + counter)
-	windowX = windowX + 5;
 	if (mainWindow != null) {
-		mainWindow.moveTo(windowX, 100);
+		let nextX = windowX + moveX;
+		let nextY = windowY + moveY;
+		if (nextX > displayWidth - windowWidth) {
+			moveX = random(-2, -8);
+			nextX = windowX + moveX;
+		}
+		if (nextX < 0) {
+			moveX = random(2, 8);
+			nextX = windowX + moveX;
+		}
+		if (nextY > displayHeight - windowHeight) {
+			moveY = random(-2, -8);
+			nextY = windowY + moveY;
+		}
+		if (nextY < 0) {
+			moveY = random(2, 8);
+			nextY = windowY + moveY;
+		}
+		windowX = nextX;
+		windowY = nextY;
+		mainWindow.moveTo(windowX, windowY);
+		// this doesn't work completely
+		mainWindow.focus();
 	}
 }
