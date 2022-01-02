d9a8011b18fe81084de3b5974755d65bffb2959f
rubrubpowah123 <jetite@gmail.com>
2022-01-02 19:17:07 +1300

    made an array of objects
    haha array go brrrrrrr

--- idiot.js
+++ idiot.js
@@ -7,56 +7,73 @@ function showMainWindow() {
 	alert("you are the idiot!")	
 }
 
-// theWindow
-// let windowName = "_blank";
-let windowName = "theWindow";
-let windowWidth = 400;
-let windowHeight = 100;
-let mainWindow = null;
-let windowX = 0;
-let windowY = 0;
+function IdiotWindow(windowName) {
+	this.windowName = windowName;
+	this.windowWidth = 400;
+	this.windowHeight = 100;
+	this.mainWindow = window.open("", this.windowName, "width=" + this.windowWidth + ",height=" + this.windowHeight);
+	this.windowX = 0;
+	this.windowY = 0;
+	this.moveX = random(2, 8);
+	this.moveY = random(2, 8);
 
-let moveX = 2;
-let moveY = 2;
+	this.move = function() {
+		let nextX = this.windowX + this.moveX;
+		let nextY = this.windowY + this.moveY;
+		if (nextX > displayWidth - this.windowWidth) {
+			this.moveX = random(-2, -8);
+			nextX = this.windowX + this.moveX;
+		}
+		if (nextX < 0) {
+			this.moveX = random(2, 8);
+			nextX = this.windowX + this.moveX;
+		}
+		if (nextY > displayHeight - this.windowHeight) {
+			this.moveY = random(-2, -8);
+			nextY = this.windowY + this.moveY;
+		}
+		if (nextY < 0) {
+			this.moveY = random(2, 8);
+			nextY = this.windowY + this.moveY;
+		}
+		this.windowX = nextX;
+		this.windowY = nextY;
+		this.mainWindow.moveTo(this.windowX, this.windowY);
+	}
+}
 
+let idiots = [];
+
+// This happens when the button is clicked
 function showDomWindow() {
-	mainWindow = window.open("", windowName, "width=" + windowWidth + ",height=" + windowHeight);
-	// mainWindow.document.write("<p>This is " + windowName + ". I am " + windowWidth + "px wide and " + windowHeight + "px tall!</p>");	
-	mainWindow.document.write("<p>YOU ARE THE IDIOT</p>");	
+	if (idiots.length < 100) {
+		let idiotNum = idiots.length + 1;
+		let curIdiot = new IdiotWindow("idiot" + idiotNum);
+		curIdiot.mainWindow.document.write("<p>YOU ARE THE IDIOT " + idiotNum + "</p>");
+		idiots.push(curIdiot);
+	};
 }
 
 function setup() {
-	moveX = random(2, 8);
-	moveY = random(2, 8);
+	// idiot1 = new IdiotWindow();
+	// idiot1.mainWindow.document.write("<p>YOU ARE THE IDIOT 1</p>");
+
+	// idiot2 = new IdiotWindow();
+	// idiot2.mainWindow.document.write("<p>YOU ARE THE IDIOT 2</p>");
 }
 
 // let counter = 0;
 function draw() {
+	for (let i=0; i<idiots.length; i+=1) {
+		let curIdiot = idiots[i];
+		curIdiot.move();
+	}
+
 	// counter = counter + 1;
 	// console.log("hello: " + counter)
-	if (mainWindow != null) {
-		let nextX = windowX + moveX;
-		let nextY = windowY + moveY;
-		if (nextX > displayWidth - windowWidth) {
-			moveX = random(-2, -8);
-			nextX = windowX + moveX;
-		}
-		if (nextX < 0) {
-			moveX = random(2, 8);
-			nextX = windowX + moveX;
-		}
-		if (nextY > displayHeight - windowHeight) {
-			moveY = random(-2, -8);
-			nextY = windowY + moveY;
-		}
-		if (nextY < 0) {
-			moveY = random(2, 8);
-			nextY = windowY + moveY;
-		}
-		windowX = nextX;
-		windowY = nextY;
-		mainWindow.moveTo(windowX, windowY);
-		// this doesn't work completely
-		mainWindow.focus();
-	}
+	// if (idiot != null) {
+	// 	idiot.move();
+	// 	// this doesn't work completely
+	// 	// mainWindow.focus();
+	// }
 }
